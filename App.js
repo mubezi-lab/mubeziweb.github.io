6fc378e2633905680d9c571de71cc0fe8874288f
Mubezi Jackson <63768744+mubezi-lab@users.noreply.github.com>
2022-11-16 07:19:02 +0300

    practice

---
+++ App.js
@@ -0,0 +1,16 @@
+function Log_on() {
+    document.getElementById("login").style.display = "block";
+  }
+  
+function Log_off() {
+  document.getElementById("login").style.display = "none";
+}
+
+function Reg_on() {
+  document.getElementById("register").style.display = "block";
+  document.getElementById("login").style.display = "none";
+}
+
+function Reg_off() {
+document.getElementById("register").style.display = "none";
+}
